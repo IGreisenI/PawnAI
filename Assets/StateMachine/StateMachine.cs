8785cb151dba86298be6d85f7890714ee7c1d09d
IGreisenI <kstjepan4@gmail.com>
2023-03-01 12:39:41 +0100

    Add comments and doc strings for documentation

--- Assets/StateMachine/StateMachine.cs
+++ Assets/StateMachine/StateMachine.cs
@@ -1,61 +1,64 @@
 using System;
 using System.Collections.Generic;
 
-// Notes
-// 1. What a finite state machine is
-// 2. Examples where you'd use one
-//     AI, Animation, Game State
-// 3. Parts of a State Machine
-//     States & Transitions
-// 4. States - 3 Parts
-//     Tick - Why it's not Update()
-//     OnEnter / OnExit (setup & cleanup)
-// 5. Transitions
-//     Separated from states so they can be re-used
-//     Easy transitions from any state
-
 public class StateMachine
 {
     private IState _currentState;
 
+    // Dictionary to hold transitions for each state
     private Dictionary<Type, List<Transition>> _transitions = new Dictionary<Type, List<Transition>>();
+
+    // List to hold transitions available in the current state
     private List<Transition> _currentTransitions = new List<Transition>();
+
+    // List to hold transitions that can be taken from any state
     private List<Transition> _anyTransitions = new List<Transition>();
 
+    // List to hold an empty set of transitions (for states with no transitions)
     private static List<Transition> EmptyTransitions = new List<Transition>(0);
 
+    /// <summary>
+    /// Advance the state machine by one tick, checking for any available transitions.
+    /// </summary>
     public void Tick()
     {
         var transition = GetTransition();
         if (transition != null)
             SetState(transition.To);
 
+        // Call the Tick() method on the current state
         _currentState?.Tick();
     }
 
     public void SetState(IState state)
     {
+        // If the new state is the same as the current state, do nothing
         if (state == _currentState)
             return;
 
+        // Call OnExit() on the current state and set the new state
         _currentState?.OnExit();
         _currentState = state;
 
+        // Get the available transitions for the new state
         _transitions.TryGetValue(_currentState.GetType(), out _currentTransitions);
         if (_currentTransitions == null)
             _currentTransitions = EmptyTransitions;
 
+        // Call OnEnter() on the new state
         _currentState.OnEnter();
     }
 
     public void AddTransition(IState from, IState to, Func<bool> predicate)
     {
+        // If there are no existing transitions for the starting state, create a new list of transitions
         if (_transitions.TryGetValue(from.GetType(), out var transitions) == false)
         {
             transitions = new List<Transition>();
             _transitions[from.GetType()] = transitions;
         }
 
+        // Add the new transition to the list for the starting state
         transitions.Add(new Transition(to, predicate));
     }
 
@@ -64,6 +67,9 @@ public class StateMachine
         _anyTransitions.Add(new Transition(state, predicate));
     }
 
+    /// <summary>
+    /// Private class to represent a transition from one state to another.
+    /// </summary>
     private class Transition
     {
         public Func<bool> Condition { get; }
@@ -76,16 +82,22 @@ public class StateMachine
         }
     }
 
+    /// <summary>
+    /// Get the first available transition, either from the current state or from any state.
+    /// </summary>
     private Transition GetTransition()
     {
+        // Check for any available transitions that can be taken from any state
         foreach (var transition in _anyTransitions)
             if (transition.Condition())
                 return transition;
 
+        // Check for any available transitions that can be taken from the current state
         foreach (var transition in _currentTransitions)
             if (transition.Condition())
                 return transition;
 
+        // If no transition is available, return null
         return null;
     }
 }
