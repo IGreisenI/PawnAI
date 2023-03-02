2785d41fe2bb5ed7feb95cb442b3ce3ebe78d42f
IGreisenI <kstjepan4@gmail.com>
2023-03-02 11:09:41 +0100

    Add state machine to PawnSingleFlyer class, transitions and states

--- Assets/PawnSingleFlyer/PawnSingleFlyer.cs
+++ Assets/PawnSingleFlyer/PawnSingleFlyer.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PawnSingleFlyer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float idleTime;
+    [SerializeField] private float partrolSpeed;
+    [SerializeField] private Transform[] patrolWaypoints;
+
+    private StateMachine _stateMachine;
+
+    void At(IState to, IState from, Func<bool> condition)
     {
-        
+        _stateMachine.AddTransition(to, from, condition);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        _stateMachine = new StateMachine();
+
+        IdleState idleState = new IdleState(transform, idleTime);
+        PatrolState patrolState = new PatrolState(transform, partrolSpeed, patrolWaypoints);
+
+        At(idleState, patrolState, FinishedIdling());
+        At(patrolState, idleState, FinishedPatrolling());
         
+        _stateMachine.SetState(idleState);
+        
+        Func<bool> FinishedIdling() => () => idleState.IdleTimeExpired();
+        Func<bool> FinishedPatrolling() => () => patrolState.PatrolFinished();
+    }
+
+    public void Update()
+    {
+        _stateMachine.Tick();
     }
 }
