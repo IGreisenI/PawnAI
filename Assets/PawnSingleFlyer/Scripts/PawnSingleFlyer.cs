c814d5f718a47c57fda65f67398995e0dad6a494
IGreisenI <kstjepan4@gmail.com>
2023-03-03 15:55:15 +0100

    Make changes to PawnSingleFlyer
    Add states ReturnToTargetState, FocusTargetState, FlyForEffectState.
    Add and cleanup conditions of transitioning.
    Add functions to switch states through the context menu of the script.
    Add model field and cache the states.

--- Assets/PawnSingleFlyer/Scripts/PawnSingleFlyer.cs
+++ Assets/PawnSingleFlyer/Scripts/PawnSingleFlyer.cs
@@ -5,43 +5,87 @@ using UnityEngine;
 
 public class PawnSingleFlyer : MonoBehaviour
 {
+    [SerializeField] private Transform model;
+
     [Header("Flight settings")]
-    [SerializeField] private float partrolSpeed;
+    [SerializeField] private float flyerSpeed;
+    [SerializeField] private float flyerRotation;
     [SerializeField] private float noiseFrequency;
     [SerializeField] private float noiseMagnitude;
 
     [Header("State settings")]
     [SerializeField] private float idleTime;
+    [SerializeField] private float flyForEffectTime;
+    [SerializeField] private Transform returnTarget;
     [SerializeField] private Transform[] patrolWaypoints;
 
     private StateMachine _stateMachine;
     private FlyerPawnMovement flyerPawnMovement;
 
-    void At(IState to, IState from, Func<bool> condition)
+    #region STATES
+    private IdleState _idle;
+    private PatrolState _patrol;
+    private ReturnToTargetState _returnToTarget;
+    private FocusTargetState _focusOnTarget;
+    private FlyForEffectState _flyForEffect;
+    #endregion
+
+    void At(IState from, IState to, Func<bool> condition)
     {
-        _stateMachine.AddTransition(to, from, condition);
+        _stateMachine.AddTransition(from, to, condition);
     }
 
     private void Awake()
     {
         _stateMachine = new StateMachine();
 
-        flyerPawnMovement = new FlyerPawnMovement(transform, noiseFrequency, noiseMagnitude);
+        flyerPawnMovement = new FlyerPawnMovement(transform, flyerSpeed, flyerRotation, noiseFrequency, noiseMagnitude);
 
-        IdleState idleState = new IdleState(transform, idleTime);
-        PatrolState patrolState = new PatrolState(transform, flyerPawnMovement, patrolWaypoints, partrolSpeed);
+        _idle = new IdleState(transform, idleTime);
+        _patrol = new PatrolState(transform, flyerPawnMovement, patrolWaypoints);
+        _returnToTarget = new ReturnToTargetState(transform, returnTarget, flyerPawnMovement);
+        _focusOnTarget = new FocusTargetState(transform, model, returnTarget, flyerPawnMovement);
+        _flyForEffect = new FlyForEffectState(transform, model, returnTarget, null, flyerPawnMovement, flyForEffectTime);
 
-        Func<bool> FinishedIdling() => () => idleState.IdleTimeExpired();
-        At(idleState, patrolState, FinishedIdling());
+        At(_idle, _patrol, _idle.IdleTimeExpired);
+        At(_patrol, _idle, _patrol.PatrolFinished);
+        At(_flyForEffect, _idle, _flyForEffect.FlightTimeExpired);
 
-        Func<bool> FinishedPatrolling() => () => patrolState.PatrolFinished();
-        At(patrolState, idleState, FinishedPatrolling());
-        
-        _stateMachine.SetState(idleState);
+        _stateMachine.SetState(_idle);
     }
 
     public void Update()
     {
         _stateMachine.Tick();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(_stateMachine != null)
+            _stateMachine.DrawStateGizmo();
+    }
+
+    [ContextMenu("ReturnToTarget")]
+    public void ReturnToTarget()
+    {
+        _stateMachine.SetState(_returnToTarget);
+    }
+
+    [ContextMenu("FlyForEffect")]
+    public void FlyForEffect()
+    {
+        _stateMachine.SetState(_flyForEffect);
+    }
+
+    [ContextMenu("FocusOnTarget")]
+    public void FocusOnTarget()
+    {
+        _stateMachine.SetState(_focusOnTarget);
+    }
+
+    [ContextMenu("Idle")]
+    public void Idle()
+    {
+        _stateMachine.SetState(_idle);
     }
 }
