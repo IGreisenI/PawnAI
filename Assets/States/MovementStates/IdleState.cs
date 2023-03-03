74e444cf2d411cb264b47284c221abb09487f6cc
IGreisenI <kstjepan4@gmail.com>
2023-03-03 15:49:17 +0100

    Add gizmos to IdleState

--- Assets/States/MovementStates/IdleState.cs
+++ Assets/States/MovementStates/IdleState.cs
@@ -7,6 +7,8 @@ public class IdleState : IState
     private Transform _transform;
     private float _idleTime; 
     private float _elapsedTime = 0f;
+    private float progressbarLength = 1f;
+    private Vector3 healthbarSize = new Vector3(1f,1f);
 
     public IdleState(Transform transform, float idleTime)
     {
@@ -35,6 +37,18 @@ public class IdleState : IState
     public void OnExit()
     {
         // When exiting the idle state, do nothing
+    }
+
+    public void DrawDebugGizmo()
+    {
+        Gizmos.DrawWireCube(_transform.position + _transform.up, healthbarSize);
+
+        progressbarLength = _elapsedTime / _idleTime;
+        if (progressbarLength > 0.66f) Gizmos.color = Color.green;
+        else if (progressbarLength > 0.33f) Gizmos.color = Color.yellow;
+        else Gizmos.color = Color.red;
+
+        Gizmos.DrawCube(_transform.position + _transform.up + new Vector3((progressbarLength / 2f) - (progressbarLength / 2f), 0, 0), new Vector3(progressbarLength, progressbarLength, healthbarSize.z));
     }
 
     public bool IdleTimeExpired()
