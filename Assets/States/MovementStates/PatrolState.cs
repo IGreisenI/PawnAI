cd5a4007aff05a65edc5cd52121dbcffeda5fc0d
IGreisenI <kstjepan4@gmail.com>
2023-03-05 13:37:26 +0100

    Add model field to PatrolState to rotate the model of the drone if needed

--- Assets/States/MovementStates/PatrolState.cs
+++ Assets/States/MovementStates/PatrolState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PatrolState : IState
 {
     private Transform _transform;
+    private Transform _model;
     private Transform[] _waypoints;
     private int _currentWaypointIndex;
     private bool _isPatrollingForward;
@@ -13,9 +14,10 @@ public class PatrolState : IState
     private Vector3 _startingPoint;
     private Vector3 from;
 
-    public PatrolState(Transform transform, Movement movement, Transform[] waypoints)
+    public PatrolState(Transform transform, Transform model, Movement movement, Transform[] waypoints)
     {
         _transform = transform;
+        _model = model;
         _waypoints = waypoints;
 
         _movement = movement;
