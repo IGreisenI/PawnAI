83b71241fb583a0ea703d7806334a21c70fe4378
IGreisenI <kstjepan4@gmail.com>
2023-03-08 16:33:18 +0100

    Smooth the speed out, smooth noise application, add obstacle spacing

--- Assets/PawnSingleFlyer/Scripts/FlyerPawnMovement.cs
+++ Assets/PawnSingleFlyer/Scripts/FlyerPawnMovement.cs
@@ -12,6 +12,11 @@ public class FlyerPawnMovement : Movement
 
     private Vector3 flyerVelocity;
 
+    public float minSpeed = 1f;
+    public float maxSpeed = 5f;
+    private float currentSpeed;
+    private float distanceFromObsticale = 2f;
+
     #region CACHE
     Vector3 direction;
     #endregion
@@ -43,17 +48,20 @@ public class FlyerPawnMovement : Movement
         float distanceToTarget = Vector3.Distance(transform.position, to);
 
         // Add perlin noise to movement
-        float noise = Mathf.PerlinNoise(Time.time * noiseFrequency, transform.position.y) * 2f - 1f;
+        float noise = Mathf.PerlinNoise(Time.time * noiseFrequency, Time.time * noiseFrequency);
 
         Vector3 noiseVector = GetRandomVector() * noise * noiseMagnitude;
         direction = Vector3.Lerp(GetForwardDirection(), (to - transform.position).normalized, curveAmount * _rotationSpeed / distanceToTarget);
-        flyerVelocity = (direction + noiseVector) * _speed;
+
+        // Speed adjustment
+        currentSpeed = Mathf.Lerp(_speed / 5f , _speed, Mathf.Abs(1 -Mathf.Clamp(Vector3.Distance(transform.position, from + (to-from)/2) / 30f, 0, 1)));
+        flyerVelocity = (direction + noiseVector) * currentSpeed;
+
+        // Collision Handling
+        DetectCollision();
+        StayAwayFromObjects();
 
         // Move towards the target position
-        if (DetectCollision())
-        {
-            flyerVelocity = transform.forward / _speed;
-        }
         transform.position += flyerVelocity * Time.deltaTime;
 
         // Rotate towards the target position
@@ -64,6 +72,7 @@ public class FlyerPawnMovement : Movement
         }
     }
 
+
     public override void ResetMovement()
     {
 
@@ -72,7 +81,8 @@ public class FlyerPawnMovement : Movement
     private Vector3 GetRandomVector()
     {
         Vector3 randomVector = Random.insideUnitSphere;
-        randomVector.y = Mathf.Abs(randomVector.y);
+        randomVector.x = 0;
+        randomVector.z = 0;
         return randomVector.normalized;
     }
 
@@ -95,10 +105,44 @@ public class FlyerPawnMovement : Movement
     {
         Ray ray = new Ray(transform.position, flyerVelocity);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, flyerVelocity.magnitude)){
-            flyerVelocity += hit.normal * flyerVelocity.magnitude;
-            return true;
+        if (Physics.Raycast(ray, out hit, flyerVelocity.magnitude))
+        {
+            if((hit.point - transform.position).magnitude < distanceFromObsticale)
+            {
+                flyerVelocity += hit.normal * (distanceFromObsticale - (hit.point - transform.position).magnitude);
+                return true;
+            }
         }
         return false;
+    }
+
+    private void StayAwayFromObjects()
+    {
+        // Cast a sphere in front of the AI to detect nearby obstacles
+        float avoidDistance = 5f;
+        float sphereRadius = 3f;
+        Collider[] colliders = Physics.OverlapSphere(transform.position + transform.forward * avoidDistance, sphereRadius);
+        if (colliders.Length == 0)
+        {
+            return;
+        }
+
+        // Calculate a steering force to avoid the obstacles
+        Vector3 avoidanceForce = Vector3.zero;
+        foreach (Collider collider in colliders)
+        {
+            Vector3 obstaclePosition = collider.transform.position;
+            Vector3 toObstacle = obstaclePosition - transform.position;
+            float distance = toObstacle.magnitude;
+            if (distance > 0f && distance < avoidDistance)
+            {
+                float weight = 1f - (distance / avoidDistance);
+                Vector3 direction = toObstacle.normalized;
+                avoidanceForce -= direction * weight;
+            }
+        }
+
+        // Apply the steering force to the velocity vector
+        flyerVelocity += avoidanceForce.normalized * _speed * Time.deltaTime;
     }
 }
